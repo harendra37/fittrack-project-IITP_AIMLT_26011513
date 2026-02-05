284a9f56a9fda6eeec85e1bcb9700b281e7d4fa8
Harendra-IITP_AIMLT_26011513 <harendra37@gmail.com>
2026-02-05 12:56:08 +0530

    feat: script for Player Score Calculator

---
+++ game_score.py
@@ -0,0 +1,8 @@
+player_name=input("Enter player name: ")
+number_of_games_played=int(input("Enter the number of games played: "))
+total_score=int(input("Enter the total score: "))
+average_score=total_score/number_of_games_played
+print(f"Player: {player_name}")
+print(f"Games Played: {number_of_games_played}")
+print(f"Total Score: {total_score}")
+print(f"Average Score: {average_score}")
